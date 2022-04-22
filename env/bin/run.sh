bbb29cb46ff36a78ad29f5af450d4c99a4ab8437
yikesoftware <1532494123@qq.com>
2022-04-22 17:37:54 +0800

    fix daemon

--- env/bin/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./kvdb -p 9999 -m 32 -l /dev/null -t 10 -d
--- env/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo docker build --tag kvdb .
-sudo docker run -it --rm -p 9999:9999 -e FLAG="minil{test_flag}" kvdb
